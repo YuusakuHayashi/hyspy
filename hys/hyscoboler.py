c15f19eac429405e136444d85c8f451a8a92e385
yuusaku.hayashi <yuusaku.hayashi@outlook.jp>
2019-04-21 19:57:52 +0900

    test

--- hys/hyscoboler.py
+++ hys/hyscoboler.py
@@ -1,7 +1,6 @@
 import re
 import sys 
 
-
 USING_STATEMENT_TEXT            = "------ [HYS-COBOL-PARSE]\n"
 PATTERN_ASTERISK                = "*"
 PATTERN_DATA_ITEM               = "^\s*[0-4][0-9]\s+[A-Z]+"
@@ -24,6 +23,11 @@ class HysCoboler:
     __environment_division__    = 1 
     __data_division__           = 2
     __procedure_division__      = 3
+
+    __identification_division_reigion__ = []
+    __environment_division_reigion__    = []
+    __data_division_reigion__           = []
+    __procedure_division_reigion__      = []
 
     parse_using_statement_flag     = True
     parse_delimited_statement_flag = True
@@ -55,27 +59,84 @@ class HysCoboler:
     __pattern_data_item_name__   = "\s+([0-9A-Z-]+)\.?\s+"
     __pattern_data_item_occurs__  = "\s+OCCURS +([0-9]+).*\."
     __pattern_data_item_pic__    = "\s+PIC\s+\(([0-9]+)\)\s+"
+    data_structure = []
 
     def __init__(self, cobol):
         self.cobol = cobol 
-        self.__parse_div__()
+        self.__get_division__()
+        self.__get_data_structure__()
 
-    def __parse_div__(self):
-        pre_lineno = "" 
+#{{{ old
+    #def __parse_div_old__(self):
+    #    pre_lineno = "" 
+    #    with open(self.cobol, 'r') as cbl:
+    #        for line in cbl:
+    #            if self.parse(PATTERN_IDENTIFICATION_DIVISION, line):
+    #                self.__start_identification_division__ = line[self.__reigion_lineno__]
+    #            if self.parse(PATTERN_ENVIRONMENT_DIVISION, line):
+    #                self.__end_identification_division__   = pre_lineno 
+    #                self.__start_environment_division__    = line[self.__reigion_lineno__]
+    #            if self.parse(PATTERN_DATA_DIVISION, line):
+    #                self.__end_environment_division__      = pre_lineno 
+    #                self.__start_data_division__           = line[self.__reigion_lineno__]
+    #            if self.parse(PATTERN_PROCEDURE_DIVISION, line):
+    #                self.__end_data_division__             = pre_lineno 
+    #                self.__start_procedure_division__      = line[self.__reigion_lineno__]
+    #            pre_lineno = line[self.__reigion_lineno__]
+#}}}
+
+    def __get_division__(self):
+        # インスタンス化時に各divisionのpropertiesの行番号の範囲をセットする
+        line_cnt = 1
         with open(self.cobol, 'r') as cbl:
             for line in cbl:
-                if self.parse(PATTERN_IDENTIFICATION_DIVISION, line):
-                    self.__start_identification_division__ = line[self.__reigion_lineno__]
-                if self.parse(PATTERN_ENVIRONMENT_DIVISION, line):
-                    self.__end_identification_division__   = pre_lineno 
-                    self.__start_environment_division__    = line[self.__reigion_lineno__]
-                if self.parse(PATTERN_DATA_DIVISION, line):
-                    self.__end_environment_division__      = pre_lineno 
-                    self.__start_data_division__           = line[self.__reigion_lineno__]
-                if self.parse(PATTERN_PROCEDURE_DIVISION, line):
-                    self.__end_data_division__             = pre_lineno 
-                    self.__start_procedure_division__      = line[self.__reigion_lineno__]
-                pre_lineno = line[self.__reigion_lineno__]
+                if self.test_reg(PATTERN_IDENTIFICATION_DIVISION, line):
+                    self.__identification_division_reigion__.append(line_cnt)
+                if self.test_reg(PATTERN_ENVIRONMENT_DIVISION, line):
+                    self.__identification_division_reigion__.append(line_cnt-1)
+                    self.__environment_division_reigion__.append(line_cnt)
+                if self.test_reg(PATTERN_DATA_DIVISION, line):
+                    self.__environment_division_reigion__.append(line_cnt-1)
+                    self.__data_division_reigion__.append(line_cnt)
+                if self.test_reg(PATTERN_PROCEDURE_DIVISION, line):
+                    self.__data_division_reigion__.append(line_cnt-1)
+                    self.__procedure_division_reigion__.append(line_cnt)
+                line_cnt+=1
+            self.__procedure_division_reigion__.append(line_cnt)
+
+# {{{
+    def __get_data_structure__old_(self):
+        line_cnt = 1
+        with open(self.cobol, 'r') as cbl:
+            for line in cbl:
+                if line_cnt >= self.__data_division_reigion__[0] and line_cnt <= self.__data_division_reigion__[1]:
+                    m = self.match_reg(self.__pattern_data_item_size1__, line)
+                    m = self.match_reg(self.__pattern_data_item_size2__, line) if not m else m
+                    m = self.match_reg(self.__pattern_data_item_size3__, line) if not m else m
+                    if m:
+                        self.data_structure.append(m[1])
+                    else:
+                        self.data_structure.append('0')
+                line_cnt+=1
+# }}}
+
+    def __get_data_structure__(self):
+        line_cnt = 1
+        with open(self.cobol, 'r') as cbl:
+            total=0
+            for line in cbl:
+                if line_cnt >= self.__data_division_reigion__[0] and line_cnt <= self.__data_division_reigion__[1]:
+                    m = self.match_reg(self.__pattern_data_item_size1__, line)
+                    m = self.match_reg(self.__pattern_data_item_size2__, line) if not m else m
+                    m = self.match_reg(self.__pattern_data_item_size3__, line) if not m else m
+                    if m:
+                        size=int(m[1])
+                        total+=size
+                        self.data_structure.append(str(total))
+                    else:
+                        self.data_structure.append('0')
+                line_cnt+=1
+        
 
     def __current_division__(self, lineno):
         if int(lineno) < int(self.__start_environment_division__):
@@ -87,6 +148,7 @@ class HysCoboler:
         else:
             return self.__procedure_division__ 
 
+#{{{
     def simple_layout(self, html_file="layout.html", css_file="layout.css"):
         i = 0
         j = 0
@@ -119,26 +181,25 @@ class HysCoboler:
                         rgna = line[self.__reigion_a__]
                         rgnb = line[self.__reigion_b__]
                         if mark == " ":
-                            if self.parse(self.__pattern_file_description__, rgna):
+                            if self.test_reg(self.__pattern_file_description__, rgna):
                                 file_name = rgnb.strip()
                                 html.write("<title>" + file_name + "</title>")
                                 html.write("<link rel='stylesheet' href='layout.css'></link>")
                                 html.write(file_name)
-                            if self.parse(self.__pattern_block_contains__, rgnb):
+                            if self.test_reg(self.__pattern_block_contains__, rgnb):
                                 html.write("<br />" + rgnb + "<br />")
-                            if self.parse(self.__pattern_file_value_of__, rgnb):
+                            if self.test_reg(self.__pattern_file_value_of__, rgnb):
                                 html.write(rgnb + "<br />")
-                            if self.parse(self.__pattern_data_01__, rgna):
+                            if self.test_reg(self.__pattern_data_01__, rgna):
                                 html.write("<div>" + rgnb + "</div>\n")
                                 #html_scal = "<div>\n"
                                 el_level = 1
                             el_class = 2
                             el_level = 0
                             el_size  = 0
-                            match = self.parseII(self.__pattern_data_item_level__, rgnb)
+                            match = self.match_reg(self.__pattern_data_item_level__, rgnb)
                             if match:
                                 el_level = int(match.group(1))
-                                #{{{
                                 #if occurs_flag:
                                 #    if data_level <= parent_level[0]:
                                 #        i = 0
@@ -150,11 +211,10 @@ class HysCoboler:
                                 #        occurs_flag = False 
                                 #else:
                                 #    html_resv = [[]]
-                                #}}}
-                                match2 = self.parseII(self.__pattern_data_item_name__, rgnb.lstrip()[2:])
+                                match2 = self.match_reg(self.__pattern_data_item_name__, rgnb.lstrip()[2:])
                                 if match2:
                                     el_name = match2.group(1)
-                                    match5 = self.parseII(self.__pattern_data_item_occurs__, rgnb)
+                                    match5 = self.match_reg(self.__pattern_data_item_occurs__, rgnb)
                                     if match5:      #OCCURS
                                         occurs_flag = True
                                         parent_level = int(match.group(1))
@@ -162,12 +222,12 @@ class HysCoboler:
                                         repeat = int(match5.group(1))
                                         html_main.append([1, 0, el_name, el_level, repeat])
                                         repeat = 0
-                                    match6 = self.parseII(self.__pattern_data_item_size3__, rgnb)
+                                    match6 = self.match_reg(self.__pattern_data_item_size3__, rgnb)
                                     if match6:
                                         el_size = int(match6.group(1))
                                         html_main.append([el_class, el_size, el_name, el_level, repeat])
                                         #i = 0
-                                        item_type = self.parseII(self.__pattern_data_item_type1__, rgnb).group(1)
+                                        item_type = self.match_reg(self.__pattern_data_item_type1__, rgnb).group(1)
                                         #while i < repeat+1:
                                         #    html_main.append([el_class, el_size, el_name, el_level, parent_level, repeat])
                                             #html_main.append([el_class, el_size, el_name])
@@ -175,16 +235,16 @@ class HysCoboler:
                                         html_scal += (("<div class='cls3'>" + item_type + "</div>")*el_size)*(repeat+1)
                                         occurs_flag = False
                                         continue
-                                    match3 = self.parseII(self.__pattern_data_item_size1__, rgnb)
+                                    match3 = self.match_reg(self.__pattern_data_item_size1__, rgnb)
                                     if match3:
                                         el_size = int(match3.group(1))
-                                        item_type = self.parseII(self.__pattern_data_item_type1__, rgnb).group(1)
+                                        item_type = self.match_reg(self.__pattern_data_item_type1__, rgnb).group(1)
                                         html_main.append([el_class, el_size, el_name, el_level, repeat])
                                         #html_main.append([el_class, el_size, el_name])
                                         #html_resv.append([el_class, el_size, el_name, el_level])
                                         html_scal += ("<div class='cls3'>" + item_type + "</div>")*el_size
                                         continue
-                                    match4 = self.parseII(self.__pattern_data_item_size2__, rgnb)
+                                    match4 = self.match_reg(self.__pattern_data_item_size2__, rgnb)
                                     if match4:
                                         el_size = len(match4.group(1))
                                         html_main.append([el_class, el_size, el_name, el_level, repeat])
@@ -200,11 +260,11 @@ class HysCoboler:
                         if item[4]:
                             repeat_arr.append([item[3], item[4]])
                             continue
-                        if item[3] =< repeat_arr[-1][0]:
-                            html_sub.
-                        else:
-                            html_sub.append(item)
-                            tmp.append(item)
+                        #if item[3] =< repeat_arr[-1][0]:
+                        #    html_sub.
+                        #else:
+                        #    html_sub.append(item)
+                        #    tmp.append(item)
 
                 #html.write("<div class='cls1'>\n")
                 #for item in html_main:
@@ -231,6 +291,7 @@ class HysCoboler:
                     #                el_size_sum += item[1]
                     #                break
                 html.write("</div>\n")
+#}}}
                                 
     def format(self, indent_level=1, out_file="format.cbl"):
         indent = 0
@@ -243,7 +304,7 @@ class HysCoboler:
                         #A領域からの文はインデントしない
                             lstriped = line[self.__reigion_b__].lstrip()
                             if self.__current_division__(line[self.__reigion_lineno__]) == self.__data_division__:
-                                if self.parse(PATTERN_DATA_ITEM, line[self.__reigion_b__]):
+                                if self.test_reg(PATTERN_DATA_ITEM, line[self.__reigion_b__]):
                                 #データ項目でのインデント
                                     indent = int(lstriped[:2]) - 2
                             indented = " "*indent + lstriped 
@@ -264,7 +325,7 @@ class HysCoboler:
                                     + line[self.__reigion_headline__] 
                                     + "\n"
                                 )
-                                if self.parse(PATTERN_PERIOD, line[self.__reigion_ab__]): 
+                                if self.test_reg(PATTERN_PERIOD, line[self.__reigion_ab__]): 
                                 #IF文等によるインデントの後判定
                                     indent = 0
                                 else:
@@ -279,33 +340,33 @@ class HysCoboler:
             with open(self.cobol, 'r') as cbl:
                 for line in cbl:
                     if mode == self.__identification_division__:
-                        if self.parse(PATTERN_IDENTIFICATION_DIVISION, line):
+                        if self.test_reg(PATTERN_IDENTIFICATION_DIVISION, line):
                             write_flag = True
-                        if self.parse(PATTERN_ENVIRONMENT_DIVISION, line):
+                        if self.test_reg(PATTERN_ENVIRONMENT_DIVISION, line):
                             break
                     if mode == self.__environment_division__:
-                        if self.parse(PATTERN_ENVIRONMENT_DIVISION, line):
+                        if self.test_reg(PATTERN_ENVIRONMENT_DIVISION, line):
                             write_flag = True
-                        if self.parse(PATTERN_DATA_DIVISION, line):
+                        if self.test_reg(PATTERN_DATA_DIVISION, line):
                             break
                     if mode == self.__data_division__:
-                        if self.parse(PATTERN_DATA_DIVISION, line):
+                        if self.test_reg(PATTERN_DATA_DIVISION, line):
                             write_flag = True
-                        if self.parse(PATTERN_PROCEDURE_DIVISION, line):
+                        if self.test_reg(PATTERN_PROCEDURE_DIVISION, line):
                             break
                     if mode == self.__procedure_division__:
-                        if self.parse(PATTERN_PROCEDURE_DIVISION, line):
+                        if self.test_reg(PATTERN_PROCEDURE_DIVISION, line):
                             write_flag = True
                     if write_flag:
                         outf.write(line)
 
-    def parse(self, pattern, string):
+    def test_reg(self, pattern, string):
         pattern = re.compile(pattern)
         if re.search(pattern, string):
             return True
         return False
 
-    def parseII(self, pattern, string):
+    def match_reg(self, pattern, string):
         pattern = re.compile(pattern)
         match = re.search(pattern, string)
         if match:
--- hys/hysfiler.py
+++ hys/hysfiler.py
@@ -3,3 +3,21 @@
 def writeHtmlHeader(CONF_FILE):
     with open(CONF_FILE, 'r', encoding='utf-8') as conf_file:
         return conf_file.read()
+
+class HysFiler:
+    def __init__(self, my_file, open_type):
+        self.type = open_type
+        self.file = open(my_file, self.type)
+
+    def copy_file(self, reigion, out_file="copy"):
+        inf = self.file
+        with open(out_file, 'w') as otf: 
+            line_cnt = 1
+            for line in inf:
+                if line_cnt >= reigion[0] and line_cnt <= reigion[1]:
+                    otf.write(line)
+                line_cnt += 1
+            inf.close()
+
+    def __del__(self):
+        self.file.close()
